0ea43774997dc520a08854351a2cf549db8f133c
renan_macbookair <renan.escalante.chong@gmail.com>
2015-07-29 21:16:03 -0400

    Add histograms microscopy

--- plot_histograms_cellasic_fig1B.m
+++ plot_histograms_cellasic_fig1B.m
@@ -12,10 +12,6 @@ dt=15/60;
 T =[0:49]*dt;
 
 %%
-figure(1)
-plot(T,area_rfp,'.-r');ylim([0 1e6])
-
-%%
 ctrs = linspace(80,1000,100);
 for i = 1:4
     for j=1:50
@@ -30,17 +26,10 @@ for i = 1:4
         med_r(i,j) = median(y);
         
     end
-    figure(2)
-    subplot(4,1,i)
-    x = squeeze(yfp_hist_conc(i,:,:));
-    h=pcolor(T,ctrs,x');set(h,'edgecolor','none');set(gca,'yscale','log');ylim([80,1000])
-    figure(3)
-    plot(T,med(i,:));hold on;set(gca,'yscale','linear');
-    
 end
 
-%% 20140701 Generate figure 1B for Mike
-close all;
+%% Plot histograms microscopy
+
 time_probe = [32]+1;
 
 TT = T(time_probe);
@@ -54,19 +43,18 @@ for i = 1:3
     figure(3)
     for j = 1:length(TT)
         subplot(4,length(time_probe),k)
-        %plot(ctrs,x(1,:),'r','linewidth',2);hold on;
         plot(ctrs,x(j,:),'k','linewidth',2);
         set(gca,'xscale','log');xlim([80,1000]);ylim([0 1.1]);
-        %title(num2str((TT(j))));
         axis square
         box off
         set(gcf,'color','None')
         set(gca,'YTick',[],'YTickLabel','')
-        
         k = k+1;
         
     end
 end
 
-filename='output/Figure_1B_microscopy_pannels';
-export_fig(filename, '-pdf','-transparent','-nocrop');
+filename='Figure_1B_microscopy_pannels';
+export_fig_specific_path(filename, '-pdf','-transparent','-nocrop');
+
+end
